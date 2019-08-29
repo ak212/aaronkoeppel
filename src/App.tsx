0f128054649ebea1875bc252e18973d529746025
Aaron Koeppel <akoeppel212@gmail.com>
2019-08-28 19:21:15 -0700

    Site now uses typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to Aaron Koeppel's website</h2>
-        </div>
-      </div>
-    );
-  }
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>
+          Welcome to Aaron Koeppel's website.
+        </h2>
+      </header>
+    </div>
+  );
 }
 
 export default App;
